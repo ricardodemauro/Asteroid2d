6c9bb342f49d05fc2bc03b197de35ae03b279201
Ricardo Mauro <ricardo.demauro@gmail.com>
2024-05-25 11:49:26 -0400

    adding latest

---
+++ scenes/background_field.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@export var planet_velocity : float = 10
+
+func _process(delta):
+	var velocit_vector = Vector2(planet_velocity, 0)
+	var new_position = $PlanetAnimated2d.global_position + velocit_vector
+	
+	$PlanetAnimated2d.global_position = new_position
